cba5531f5605cf73ec1edb15d62000bfefba679a
PraveenDV <69348358+PraveenDV@users.noreply.github.com>
2023-11-01 17:51:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,71 @@
+from flask import Flask, jsonify, request
+import pandas as pd
+
+movies_data = pd.read_csv('final.csv')
+
+app = Flask(__name__)
+
+# extracting important information from dataframe
+all_movies=movies_data[['original_title', 'poster_link', 'release_date', 'runtime', 'weighted_rating']]
+
+# variables to store data
+liked_movies=[]
+not_liked_movies=[]
+did_not_watch=[]
+# method to fetch data from database
+def fetch():
+  m_data={
+    'original_title': all_movies.iloc[0,0],
+    'poster_link': all_movies.iloc[0,1],
+    'release_date': all_movies.iloc[0,2] or 'N/A',
+    'duration': all_movies.iloc[0,3],
+    'rating': all_movies.iloc[0,4]/2
+  }
+  return m_data
+
+# /movies api
+@app.route('/movies')
+def get_movie():
+  movie_data=fetch()
+  return jsonify({
+    "data":movie_data,
+    "success":'success'
+  })
+
+# /like api
+@app.route('/like', methods=['POST'])
+def liked_movie():
+  global all_movies
+  movie_data=fetch()
+  liked_movies.append(movie_data)
+  all_movies.drop([0], inplace=True)
+  all_movies=all_movies.reset_index(drop=True)
+  return jsonify({
+    "status":'success'
+  })
+# /dislike api
+@app.route('/dislike', methods=['POST'])
+def disliked_movie():
+  global all_movies
+  movie_data=fetch()
+  not_liked_movies.append(movie_data)
+  all_movies.drop([0], inplace=True)
+  all_movies=all_movies.reset_index(drop=True)
+  return jsonify({
+    "status":'success'
+  })
+
+# /did_not_watch api
+@app.route('/did_not_watch', methods=['POST'])
+def did_not_watch_movie():
+  global all_movies
+  movie_data=fetch()
+  did_not_watch.append(movie_data)
+  all_movies.drop([0], inplace=True)
+  all_movies=all_movies.reset_index(drop=True)
+  return jsonify({
+    "status":'success'
+  })
+
+if __name__ == "__main__":
+  app.run()
